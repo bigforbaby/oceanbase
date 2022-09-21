b62396c63772d9cd50b89414bec8b0f9deb98336
lzc <aa@souhu.com>
2022-09-21 15:46:42 +0800

    update

--- gmall-realtime/src/main/java/com/atguigu/gmall/realtime/common/Constant.java
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/common/Constant.java
@@ -17,4 +17,5 @@ public class Constant {
     public static final String TOPIC_DWD_TRAFFIC_ERR = "dwd_traffic_err";
     public static final String TOPIC_DWD_TRAFFIC_PAGE = "dwd_traffic_page";
     public static final String TOPIC_DWD_TRADE_CART_ADD = "dwd_trade_cart_add";
+    public static final String TOPIC_DWD_TRADE_ORDER_DETAIL = "dwd_trade_order_detail";
 }
