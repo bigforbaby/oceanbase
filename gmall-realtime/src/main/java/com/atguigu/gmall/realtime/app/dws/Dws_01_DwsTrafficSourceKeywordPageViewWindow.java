fdb7b76495f7ee3bc74f8dfd3f8cdc8ade1992b0
lzc <aa@souhu.com>
2022-09-26 13:49:00 +0800

    update

---
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dws/Dws_01_DwsTrafficSourceKeywordPageViewWindow.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.realtime.app.dws;
+
+/**
+ * @Author lzc
+ * @Date 2022/9/26 13:48
+ */
+public class Dws_01_DwsTrafficSourceKeywordPageViewWindow {
+    public static void main(String[] args) {
+    
+    }
+}
+/*
+
+*/
---
+++ gmall-realtime/src/test/java/com/atguigu/Test4.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Author lzc
+ * @Date 2022/9/25 10:31
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        // 两个整数相除, 分母不能是 0
+        System.out.println(0.0/0==0.0/0);
+        System.out.println(1.0/0);
+    }
+}
