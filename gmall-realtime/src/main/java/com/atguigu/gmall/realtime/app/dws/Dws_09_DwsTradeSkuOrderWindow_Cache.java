2efc8f32afe84617b9aa920329ab4ecae96d220c
lzc <aa@souhu.com>
2022-09-30 09:29:20 +0800

    update

--- gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dws/Dws_09_DwsTradeSkuOrderWindow_Cache.java
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dws/Dws_09_DwsTradeSkuOrderWindow_Cache.java
@@ -35,12 +35,12 @@ import java.util.HashSet;
  * @Author lzc
  * @Date 2022/9/29 11:18
  */
-public class Dws_09_DwsTradeSkuOrderWindow extends BaseAppV1 {
+public class Dws_09_DwsTradeSkuOrderWindow_Cache extends BaseAppV1 {
     public static void main(String[] args) {
-        new Dws_09_DwsTradeSkuOrderWindow().init(
+        new Dws_09_DwsTradeSkuOrderWindow_Cache().init(
             4009,
             2,
-            "Dws_09_DwsTradeSkuOrderWindow",
+            "Dws_09_DwsTradeSkuOrderWindow_Cache",
             Constant.TOPIC_DWD_TRADE_ORDER_DETAIL
         );
     }
@@ -221,7 +221,7 @@ public class Dws_09_DwsTradeSkuOrderWindow extends BaseAppV1 {
                     JSONObject rowOpTsMaxData = rowOpTsMaxDataState.value();
                     
                     if (rowOpTsMaxData == null) {
-                        long ts = ctx.timerService().currentProcessingTime();
+                        long ts = obj.getLong("ts") * 1000;
                         // 当第一条来的时候注册定时器
                         ctx.timerService().registerProcessingTimeTimer(ts + 5000);
                         
@@ -254,3 +254,115 @@ public class Dws_09_DwsTradeSkuOrderWindow extends BaseAppV1 {
         
     }
 }
+/*
+补充维度信息:
+    每来一条数据, 就需要查询一次6 张维度表
+    sku_id = 10
+        多次频繁的使用, 多次去查询数据.
+        影响: 1. 影响实时的实效性 2. 对数据库也有一定的压力
+        
+优化 1:
+    缓存(内存)
+    先查缓存, 缓存没有,再去查数据库, 然后存储到缓存中
+a: flink 的状态中(堆内存)
+    好处:
+        1. 本地内存. 所以不经过网络, 读写速度特别快
+        2. 状态有多种数据结果供选择, 使用也比较方便
+        
+    坏处:
+        1. 额外占据 flink 的内存, 影响 flink 的计算
+        2. 当维度发生变化的时候, 没有办法及时更新
+
+b: redis 旁路缓存
+    好处:
+        1. 当维度发生变化的时候,可以及时更新
+     
+     坏处:
+        1. 访问 redis 经过, 相比堆内存, 要慢
+        2. 堆 redis 的内存也有一定的压力
+        
+----------------------------
+redis 中数据结构的选择
+1. string  选择这种数据
+key             value
+table+id        json格式的字符串
+sku:10        {"id": 10, "sku_name": "", ...}
+
+优点:
+    1. 读取特点方便
+    2. 方便给每条数据单独设置 ttl
+缺点:
+    每条维度都需要占用一个 key, key 过多
+    单独的放入一个库中 4 号
+   
+2. list
+key                 value
+table               {}, {}, {},....
+sku_id              ...
+缺点:
+    1. 无法单独给每条数据设置 ttl
+    2. 写比较方便, 查的时候需要全部独处, 遍历
+
+2. set
+也不能
+
+4. hash
+key                 field   value
+table               id      {...}
+
+优点:
+    1. key 有限
+    2. 读取方便
+缺点:
+    没有办法单独对每条维度数据设置 ttl
+
+5. zset
+
+
+
+
+------
+
+sku 粒度
+    多了一些维度信息
+
+1. 去重
+    为什么会有重复数据?
+     在 dwd 层下单事务事实表中有 left join
+    
+    按照订单详情 id 来去重
+     a: 使用 session 窗口
+        实现性比较低: 最后一条数据到来之后 5s 才会出结果
+     b: 定时器
+        使用处理时间定时器
+            第一条数据来了之后 5s 后出结果
+     c: 如果用到右表的数据, 直接取第一条数据
+        第一条来了之后直接出结果
+2. 解析成 pojo
+    封装到 pojo
+        builder
+        
+3. 开窗聚合
+    keyBy   开窗 聚合
+    
+4. 补充维度信息
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
